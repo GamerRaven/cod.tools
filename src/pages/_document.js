8466c27484b514f7f8f99704e5f9de13a54f96e5
GamerRaven <jwang@ravenaro.com>
2023-05-20 19:23:43 -0700

    Built File Structure and Basic Styling

---
+++ src/pages/_document.js
@@ -0,0 +1,19 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <title>Call of Dragons Tools</title>
+        <meta
+          name="description"
+          content="The Best and most Trusted Complete Courses"
+        ></meta>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
