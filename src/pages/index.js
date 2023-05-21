0f8557571320967ecb27520c7611e667019dbfb1
GamerRaven <jwang@ravenaro.com>
2023-05-21 14:09:13 -0700

    Added Home Page
    Made basic layouts in Home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,40 @@
-import Header from "@/components/header/header"
+import Link from 'next/link';
+import './index.css';
+import Header from '@/components/header/header';
 
-export default function Home() {
+const Home = () => {
+  const sections = [
+    { 
+      path: '/page1', 
+      title: 'Section 1', 
+      description: 'Description for section 1' 
+    },
+    { 
+      path: '/page2', 
+      title: 'Section 2', 
+      description: 'Description for section 2' 
+    },
+  ];
+
   return (
     <>
-    <Header pageTitle="Home" />
-      <h1>Hello World</h1>
+      <Header />
+      <div className="container">
+        <h1 className="title">Welcome to CoD Tools</h1>
+        <h3 className="subtitle">Subtitle goes here</h3>
+        <div className="sections">
+          {sections.map(section => (
+            <Link key={section.path} href={section.path}>
+              <a className="section">
+                <h2 className="section-title">{section.title}</h2>
+                <p className="section-description">{section.description}</p>
+              </a>
+            </Link>
+          ))}
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
+
+export default Home;
